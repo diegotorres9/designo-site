e0e4efef7cd76d7a006790af99cbe02b76e62b97
Diego Torres <diegoetorres1@gmail.com>
2026-07-07 13:09:23 -0400

    swapping between icons when opening mobile menu

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,9 +1,14 @@
 const menuIcon = document.querySelector("#mobile-menu-button");
 const mobileMenu = document.querySelector(".mobile-menu");
 const overlay = document.querySelector(".overlay");
+const hamburgerIcon = document.querySelector(".open-menu-icon");
+const closeIcon = document.querySelector(".close-menu-icon");
+
 
 menuIcon.addEventListener('click', () => {
     mobileMenu.classList.toggle('menuIsOpen');
     overlay.classList.toggle('overlay-show');
+    hamburgerIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
     // console.log('event worked, class toggle did not')
 })
